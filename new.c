5b3ec009d7baa796e42398b4874a0c2d79fbcd66
Sharko47 <sksharukh4@gmail.com>
2019-11-10 20:05:29 +0530

    Digit Addition without Modulus

--- new.c
+++ new.c
@@ -1,14 +1,17 @@
-#include <stdio.h>    
-void main (){  
-   FILE *fp;  
-   int length;  
-   
-   fp = fopen("FileHandling.c", "r");  
-   fseek(fp, 0, SEEK_END);  
-  
-   length = ftell(fp);  
-  
-   fclose(fp);  
-   printf("Size of file: %d bytes", length);  
-   
+#include <stdio.h>
+int main()
+{
+   int c, sum, t;
+   char n[1000];
+   printf("Enter the number?\t");
+   scanf("%s", n);
+   sum = c = 0;
+   while (n[c] != '\0')
+   {
+      t = n[c] - '0';
+      sum = sum + t;
+      c++;
+   }
+   printf("Sum of %s = %d\n",n,sum);
+   return 0;
 }
