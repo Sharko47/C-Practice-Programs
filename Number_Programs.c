53e194559119c84250066975ccd6cc49cb352c4b
Sharukh Shaikh <sksharukh4@gmail.com>
2019-11-10 16:59:53 +0530

    Program Updated

--- Number_Programs.c
+++ Number_Programs.c
@@ -1,6 +1,6 @@
-#include<stdio.h>
-#include<string.h>
-#include"Number_ProgramsHeader.h"
+#include <stdio.h>
+#include <string.h>
+#include "Number_ProgramsHeader.h"
 int main(int argc, char const *argv[])
 {
     /* code */
@@ -8,15 +8,17 @@ int main(int argc, char const *argv[])
     printf("Choose Any One:\n1.Program to find a length of a Number.\n2.Program to find a number is a Disarium number of not.\n");
     printf("3.Program to Check Happy Number.\n4.Program to print all Pronic numbers between 1 To N.\n5.Program to Check Deficient Number.\n");
     printf("Choice->\t");
-    scanf("%d",&choice);
-    if(choice<=0){
+    scanf("%d", &choice);
+    if (choice <= 0)
+    {
         printf("Sorry!! Choice cannot be Negative or '0'.\n");
     }
-    else if(choice == 1){
+    else if (choice == 1)
+    {
         printf("\t\t\tYour Choice was:1.Program to find a length of a Number.\n");
-        extern  int number;
+        extern int number;
         printf("Enter the Number:\t");
-        scanf("%d",&number);
+        scanf("%d", &number);
         Find_Numbers_Length(number);
     }
     else if (choice == 2)
@@ -25,13 +27,15 @@ int main(int argc, char const *argv[])
         printf("\t\t\tYour Choice was:2.Program to find a number is a Disarium number of not.\n");
         extern int disarium_number;
         printf("Enter the Number to Check if it's Disarium Number or Not:\t");
-        scanf("%d",&disarium_number);
-        if(disarium_number<0){
-        printf("You've entered a negative number!!\n");
-        }else
+        scanf("%d", &disarium_number);
+        if (disarium_number < 0)
         {
-        Check_Disarium_Number(disarium_number);
-        } 
+            printf("You've entered a negative number!!\n");
+        }
+        else
+        {
+            Check_Disarium_Number(disarium_number);
+        }
     }
     else if (choice == 3)
     {
@@ -39,7 +43,7 @@ int main(int argc, char const *argv[])
         printf("\t\t\tYour Choice was:3.Program to Check Happy Number.\n");
         extern int happy_number;
         printf("Enter the Number:\t");
-        scanf("%d",&happy_number);
+        scanf("%d", &happy_number);
         Check_Happy_Number(number);
     }
     else if (choice == 4)
@@ -47,7 +51,7 @@ int main(int argc, char const *argv[])
         printf("\t\t\tYour Choice was:4.Program to print all Pronic numbers between 1 To N.\n");
         extern int pronic_range;
         printf("Enter the Number to Print all Pronic Numbers upto to \"Number\":\t");
-        scanf("%d",&pronic_range);
+        scanf("%d", &pronic_range);
         Print_Pronic_Numbers(pronic_range);
     }
     else if (choice == 5)
@@ -55,11 +59,9 @@ int main(int argc, char const *argv[])
         printf("\t\t\tYour Choice was:5.Program to Check Deficient Number.\n");
         extern int deficient_number;
         printf("Enter the Number:\t");
-        scanf("%d",&deficient_number);
+        scanf("%d", &deficient_number);
         Check_Deficient_Number(deficient_number);
     }
-    
-    
-    
+
     return 0;
 }
--- Number_ProgramsHeader.h
+++ Number_ProgramsHeader.h
@@ -1,106 +1,124 @@
-#include<stdio.h>
-#include<math.h>
+#include <stdio.h>
+#include <math.h>
 
-#define Power(num) (int)powf(num,2)
+#define Power(num) (int)powf(num, 2)
 
-int disarium_number;//Global Variable for Checking if a number is Disarium Number of not.
+int disarium_number; //Global Variable for Checking if a number is Disarium Number of not.
 
-int number;//Global Variable for Find a length of any received number.
+int number; //Global Variable for Find a length of any received number.
 
-int happy_number;//Global Variable for Checking if a number is a Happy Number or not.
+int happy_number; //Global Variable for Checking if a number is a Happy Number or not.
 
-int pronic_range;//Global Variable for Printing 1 To N Pronic Numbers.
+int pronic_range; //Global Variable for Printing 1 To N Pronic Numbers.
 
-int deficient_number;//Global Variable for Checking if a number is a Deficient number or not.
+int deficient_number; //Global Variable for Checking if a number is a Deficient number or not.
 
-void Check_Disarium_Number(){
-int count = 0,counter,rem,final_result=0;
-counter = disarium_number;
-while(counter>0){//Findind the length of a number.
-counter = counter/10;
-++count;
-}
-printf("Number of Digits = %d\n",count);
-int disarium_copy = disarium_number;
-while(disarium_number>0){//Calculating the sum of digits powered with their respective position.
-    rem = disarium_number%10;
-    final_result = final_result + (int)powf(rem,count);
-    disarium_number = disarium_number/10;
-    count--;
-}
-if(final_result == disarium_copy){//Condition to check, if a final result is same as a received number then it's a Disarium Number.
-printf("%d is a Disarium Number = %d\n",disarium_copy,final_result);
-}else
+void Check_Disarium_Number()
 {
-    /* code */
-    printf("%d is not a Disarium Number\n",disarium_copy);
-}
+    int count = 0, counter, rem, final_result = 0;
+    counter = disarium_number;
+    while (counter > 0)
+    { //Findind the length of a number.
+        counter = counter / 10;
+        ++count;
+    }
+    printf("Number of Digits = %d\n", count);
+    int disarium_copy = disarium_number;
+    while (disarium_number > 0)
+    { //Calculating the sum of digits powered with their respective position.
+        rem = disarium_number % 10;
+        final_result = final_result + (int)powf(rem, count);
+        disarium_number = disarium_number / 10;
+        count--;
+    }
+    if (final_result == disarium_copy)
+    { //Condition to check, if a final result is same as a received number then it's a Disarium Number.
+        printf("%d is a Disarium Number\n", disarium_copy);
+    }
+    else
+    {
+        /* code */
+        printf("%d is not a Disarium Number\n", disarium_copy);
+    }
 }
 
-void Find_Numbers_Length(){
-int length = 0;
-while(number>0){
-number = number/10;
-++length;
-}
-printf("Length of a Number = %d\n",length);
-}
-
-
-int IsHappyNumber(int temp_num){      //Function of calculating the sum of Happy Number.
-int remain,sum = 0;
-while(temp_num>0){
-    remain = temp_num%10;
-    sum = sum + Power(remain);//Power is a Macro Function which is declared on the top.
-    temp_num = temp_num/10; 
-}
-return sum;
-}
-
-void Check_Happy_Number(){
-int number_copy;
-number_copy = happy_number;
-while (number_copy!=1 && number_copy != 4)
+void Find_Numbers_Length()
 {
-    number_copy = IsHappyNumber(number_copy);//Calling a Function to get a value in return.
+    int length = 0;
+    while (number > 0)
+    {
+        number = number / 10;
+        ++length;
+    }
+    printf("Length of a Number = %d\n", length);
 }
-if(number_copy == 1){
-    printf("%d is a Happy Number\n",happy_number);
-}else if (number_copy ==4)
+
+int IsHappyNumber(int temp_num)
+{ //Function of calculating the sum of Happy Number.
+    int remain, sum = 0;
+    while (temp_num > 0)
+    {
+        remain = temp_num % 10;
+        sum = sum + Power(remain); //Power is a Macro Function which is declared on the top.
+        temp_num = temp_num / 10;
+    }
+    return sum;
+}
+
+void Check_Happy_Number()
 {
-    printf("%d is not a Happy Number\n",happy_number);
-}
+    int number_copy;
+    number_copy = happy_number;
+    while (number_copy != 1 && number_copy != 4)
+    {
+        number_copy = IsHappyNumber(number_copy); //Calling a Function to get a value in return.
+    }
+    if (number_copy == 1)
+    {
+        printf("%d is a Happy Number\n", happy_number);
+    }
+    else if (number_copy == 4)
+    {
+        printf("%d is not a Happy Number\n", happy_number);
+    }
 }
 
-
-int check_Pronic_Number(int number){//A Function to Check whether received number is Pronic Number or Not.
-    int temp,i;
-    for(i = 1;i<=number;i++){
-        temp = i * (i-1);
-        if(temp == number){
+int check_Pronic_Number(int number)
+{ //A Function to Check whether received number is Pronic Number or Not.
+    int temp, i;
+    for (i = 1; i <= number; i++)
+    {
+        temp = i * (i - 1);
+        if (temp == number)
+        {
             break;
         }
     }
     return temp;
 }
 
-void Print_Pronic_Numbers(){
-    register int i = 2,result=0;
-    printf("The Pronic Numbers Upto %d:\n",pronic_range);
-    for(;i<=pronic_range;i++){
-         result = check_Pronic_Number(i);//Calling an above function to find pronic Numbers.
-         if(result == i){
-             printf("%d\t",result);
-         }
+void Print_Pronic_Numbers()
+{
+    register int i = 2, result = 0;
+    printf("The Pronic Numbers Upto %d:\n", pronic_range);
+    for (; i <= pronic_range; i++)
+    {
+        result = check_Pronic_Number(i); //Calling an above function to find pronic Numbers.
+        if (result == i)
+        {
+            printf("%d\t", result);
+        }
     }
-    
 }
 
-void Check_Deficient_Number(){
+void Check_Deficient_Number()
+{
     register int i = 1;
-    int divisor,sum = 0;
-    for(;i<deficient_number;i++){
-
+    int divisor, sum = 0, deficient_copy;
+    deficient_copy = deficient_copy;
+    for (; i < deficient_number; i++)
+    {
+        
     }
 }
 #undef Power
