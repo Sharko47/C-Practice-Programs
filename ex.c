16327eb31046a66f873f504b365531fc7640b867
Sharko47 <sksharukh4@gmail.com>
2020-01-28 00:49:44 +0530

    Extra new files added Log:27/01/2020

--- ex.c
+++ ex.c
@@ -2,38 +2,14 @@
 #include <string.h>
 #include <stdlib.h>
 int main(int argc, char const *argv[])
-{   /*
-    //Given Comma Separated digits, print the number represented by the digits.
-    //Constraint : If number contains even number of negative integers then print number without negative sign.
-                   Else print number with negative sign.
-    Input: 1-> 0,6,9,8
-    Output: 698
-    Input: 2-> 9,-5,65
-    Output: 9-565
-    Input: 3-> 0,-9,98,8,-6
-    Output: 99886
-    */
-    char input[50];
-    gets(input);
-    int len = strlen(input), i;
-    static int temp,neg_c;
-    for(i =0;i<len;i++){
-        if(input[i] == 45){
-            neg_c++;
-        }
-    }
-    for (i = 0; i<len; i++)
-    {
-        temp = input[i] - 48;
-        if (input[i] == 44 || (temp==0 && i==0))
-        {
-          continue;
-        }else if(input[i] == 45){
-            if(neg_c%2!=0){
-                printf("%c",input[i]);
-            }
-            continue;
-        }
-        printf("%d",temp);
-    }
+{   
+    //Program to print String length and number of digits in a number without using any builtin function.
+    //%n is used to find the number of characters used.
+    int n,len,str_len;
+    char str[100];
+    scanf("%d%n\n",&n,&len);
+    scanf("%s%n",str,&str_len);
+    printf("Number of Digits in '%d' : %d\n",n,len);
+    printf("Length of '%s' : %d",str,str_len);
+    return 0;
 }
